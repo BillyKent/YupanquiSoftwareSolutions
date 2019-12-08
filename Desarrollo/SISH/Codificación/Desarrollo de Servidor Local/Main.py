cd1b1515bb946dfdaa28029bf4bee4eb5d707f50
LaRosaSanchez <carlos.lrsa@hotmail.com>
2019-12-08 09:22:03 -0500

    Cambios generales de seguridad de nube

--- Desarrollo/SISH/Codificación/Desarrollo de Servidor Local/Main.py
+++ Desarrollo/SISH/Codificación/Desarrollo de Servidor Local/Main.py
@@ -25,7 +25,7 @@ def grabar():
         camara.get_frame()
         print "frame"
 
-def prueba():
+def apertura():
     controladorApertura = ControladorApertura()
     while True:
         controladorApertura.VerificarSolicitudApertura(reconocedor.ObtenerRostros())
@@ -33,8 +33,8 @@ def prueba():
 if __name__ == '__main__':
     
     reconocedor = ReconocedorRostros()
-    threading.Thread(target = prueba).start()
-    threading.Thread(app.run(host='192.168.1.13', threaded = True, debug=False)).start()
+    threading.Thread(target = apertura).start()
+    threading.Thread(app.run(host='', threaded = True, debug=False)).start()
 
 
         
