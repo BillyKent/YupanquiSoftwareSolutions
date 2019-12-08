1b9c9a29e8d58b55c909dbe4d72eca77798409e7
Luis Huayta <luis.huayta6@unmsm.edu.pe>
2019-12-07 19:21:09 -0500

    Agregando funciones

--- Desarrollo/SISH/Codificación/Desarrollo de Servidor en la Nube/SISH_SN/routes/users.js
+++ Desarrollo/SISH/Codificación/Desarrollo de Servidor en la Nube/SISH_SN/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+const userController = require('../controllers/user.controller');
+
+/* User endpoints */
+router.get('/all', userController.allUsers);
+router.get('/userByEmail', userController.getUserByEmail);
+router.put('/update',userController.updateUser);
+router.delete('/delete', userController.deleteUser);
 
 module.exports = router;
